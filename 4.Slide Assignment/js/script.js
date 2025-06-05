1f666cdbd84961e95985eef2633b553d086db749
훙_Hung <hungnd@hivelab.com>
2025-06-05 15:32:29 +0700

    update

---
+++ 4.Slide Assignment/js/script.js
@@ -0,0 +1,16 @@
+// Swiper library
+const swiper = new Swiper(".product_slides", {
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  touchReleaseOnEdges: true,
+  breakpoints: {
+    768: {
+      spaceBetween: 24,
+    },
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  mousewheel: true,
+});
